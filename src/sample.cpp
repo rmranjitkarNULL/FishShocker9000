0b46007319e73ee8449398c56343ed6b5ecac63e
rmranjitkar <rmranjitkar@gmail.com>
2025-06-26 13:10:59 -0400

    added fifo (untested) source code to code base

---
+++ src/sample.cpp
@@ -0,0 +1,33 @@
+#include "sample.h"
+
+void cb_init(CircularBuffer *cb, uint8_t *buffer_array, uint8_t size){
+    cb->buffer = buffer_array;
+    cb->head = 0;
+    cb->tail = 0;
+    cb->size = size;
+    cb->full = false;
+}
+
+bool cb_push(CircularBuffer *cb, uint8_t data){
+    if(cb_is_full(cb)) return false;
+    cb->buffer[cb->head] = data;
+    cb->head = (cb->head + 1) % cb-> size;
+    cb->full = (cb->head == cb->tail);
+    return true;
+}
+
+bool cb_pop(CircularBuffer *cb, uint8_t *data){
+    if(cb_is_empty(cb)) return false;
+    *data = cb->buffer[cb->tail];
+    cb->tail = (cb->tail + 1) % cb->size;
+    cb->full = false;
+    return true;
+}
+
+bool cb_is_empty(CircularBuffer *cb) {
+    return (!cb->full && cb->head == cb->tail);
+}
+
+bool cb_is_full(CircularBuffer *cb) {
+    return cb->full;
+}
